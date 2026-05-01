8149957a01900c446ab4578097ae3b6de0bf0fe3
amaoliu <hiytom@gmail.com>
2026-05-01 16:45:21 +0800

    Reset scroll on route changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import BackToTop from './components/BackToTop';
@@ -8,6 +8,22 @@ import ApronsPage from './pages/ApronsPage';
 import ChefWearPage from './pages/ChefWearPage';
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual';
+    }
+  }, []);
+
+  useEffect(() => {
+    if (location.hash) {
+      return;
+    }
+
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [location.pathname, location.hash]);
+
   return (
     <div id="app-root" className="min-h-screen bg-white">
       <Navbar />
